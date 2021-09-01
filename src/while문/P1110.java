cab2a936743d4a9c63e5ac009b8e90aed03672e7
mnngy <59257197+mnngy@users.noreply.github.com>
2021-09-01 20:04:28 +0900

    백준 알고리즘 스터디

--- src/while문/P1110.java
+++ src/while문/P1110.java
@@ -7,23 +7,19 @@ public class P1110 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt(); // 26
+        int N = scanner.nextInt();
         scanner.close();
 
-        int A; // 10의 자리
-        int B; // 1의 자리
-        int C; // 새로운 수 1의 자리
-        int result = 0; // 새로운 수
-        int cycle = 1; // 사이클 수
+        int cnt = 0;
+        int copy = N;
 
-        while (N != result) {
-            A = N / 10; // 2
-            B = N % 10; // 6
-            C = (A + B) % 10; // 8
-            result = (10 * B) + C; // 68
-            N = result;
-            ++cycle;
+        while (true) {
+            N = ((N%10)*10) + (((N/10)+(N%10))%10);
+            cnt++;
+
+            if (N == copy)
+                break;
         }
-        System.out.println(cycle);
+        System.out.println(cnt);
     }
 }
--- src/배열/P10818.java
+++ src/배열/P10818.java
@@ -1,9 +1,11 @@
+package 배열;
+
 import java.util.Scanner;
 
 // https://www.acmicpc.net/problem/10818
 public class P10818 {
 
     public static void main(String[] args) {
-        
+
     }
 }
